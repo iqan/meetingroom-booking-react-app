7f99e66bd45c7918e946f0b69c6c45b379871cd1
iqans <iqan.shaikh@gmail.com>
2019-04-22 19:02:48 +0530

    redux in login WIP

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
+import { login } from "../redux/actions/userActions";
 
-export default class Login extends Component {
+class Login extends Component {
+  user = this.props.user;
+  handleLogin = e => {
+    e.preventDefault();
+    this.props.onLogin(this.user);
+  };
   render() {
     return (
       <div className="card">
@@ -14,6 +22,7 @@ export default class Login extends Component {
               id="email"
               aria-describedby="email"
               placeholder="Enter email"
+              onChange={e => (this.user.email = e.target.value)}
             />
             <small className="form-text text-muted" />
           </div>
@@ -24,9 +33,14 @@ export default class Login extends Component {
               className="form-control"
               id="password"
               placeholder="Password"
+              onChange={e => (this.user.password = e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.handleLogin}
+          >
             Login
           </button>
         </form>
@@ -34,3 +48,26 @@ export default class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+function mapDispatchToProp(dispatch) {
+  return {
+    onLogin: user => {
+      dispatch(login(user));
+    }
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProp
+)(Login);
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -8,6 +8,7 @@ export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.DO_LOGIN:
     case actionTypes.DO_REGISTER:
+      alert("user :" + JSON.stringify(action.user));
       return {
         ...state,
         user: action.user
